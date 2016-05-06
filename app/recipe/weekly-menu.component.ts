967ba1022234f861b48d61aecd7cbc7e86c97d53
Tiberiu Covaci <tibi@covaci.se>
2016-05-06 13:08:55 +0100

    Inject serviece locally

--- app/recipe/weekly-menu.component.ts
+++ app/recipe/weekly-menu.component.ts
@@ -5,15 +5,15 @@ import {IRecipe, RecipeService} from './recipe.service';
 @Component({
     selector: 'weekly-menu',
     templateUrl: './app/recipe/weekly-menu.component.html',
-    pipes: [ImagePipeTransform]
+    pipes: [ImagePipeTransform],
+    providers: [RecipeService]
 })
 export class WeeklyMenuComponent {
     weekDays = ['Moday', 'Tuesday', 'Wednesday'];
 
     recipes: IRecipe[];
 
-    constructor() {
-        let service = new RecipeService();
+    constructor(service : RecipeService) {
         this.recipes = service.getRecipes().map((r, i) => {
                 r.weekDay = this.weekDays[i % this.weekDays.length];
                 return r;
