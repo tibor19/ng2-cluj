d17a3f9d510d3a76fd75bb69d264f9ed5326a256
Tiberiu Covaci <tibi@covaci.se>
2016-05-06 12:38:20 +0100

    Add week day into the template

--- app/recipe/weekly-menu.component.ts
+++ app/recipe/weekly-menu.component.ts
@@ -1,7 +1,41 @@
 import {Component} from 'angular2/core';
 
+interface IRecipe {
+    recipeId: number;
+    name: string;
+    price: number;
+    time: number;
+    image: string;
+    weekDay?: string;
+}
+
 @Component({
     selector: 'weekly-menu',
-    templateUrl : './app/recipe/weekly-menu.component.html'
+    templateUrl: './app/recipe/weekly-menu.component.html'
 })
-export class WeeklyMenuComponent{}
+export class WeeklyMenuComponent {
+    weekDays = ['Moday', 'Tuesday', 'Wednesday'];
+
+    recipes: IRecipe[];
+
+    constructor() {
+        this.recipes = [{
+            "recipeId": 1,
+            "name": "Fish sticks and Rice",
+            "price": 2.0,
+            "time": 20,
+            "image": "fishsticks-mine"
+        }, {
+                "recipeId": 2,
+                "name": "Pizza",
+                "price": 13.0,
+                "time": 30,
+                "image": "pizza-clip"
+            }].map((r: IRecipe, i: number) => {
+                r.weekDay = this.weekDays[i % this.weekDays.length];
+                return r;
+            });
+    }
+
+
+}
